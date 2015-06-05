b9920f2d2ea7987769861fa87377a7c75c9cc072
srmds <stevenramdas@gmail.com>
2015-06-05 21:23:30 +0200

    config output path, refactor code

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,9 @@ var request = require('request');
 var $ = require('cheerio');
 var config = require('config');
 var pretty = require('../utils/pretty');
-var cronJob = require('cron').CronJob;
+var CronJob = require('cron').CronJob;
 var moment = require('moment');
+var mkdirp = require('mkdirp');
 
 router.get('/', function(req, res) {
     res.sendFile('events-sample.html', { root: path.join(__dirname, '../public') });
@@ -17,49 +18,57 @@ router.get('/', function(req, res) {
  * Cron job runs every minute, for now, for production properly setup schedule.
  * More info: http://crontab.org
 */
-new cronJob('*/60 * * * * *', function() {
-    url = config.get('scrapeResources')[0];
-        var items = [];
-    console.log(moment().format() + ' cron job for: ' + url);
-    request(url, function(err, resp, html){
-        if(err) {
-            throw err;
-        } else {
-            var parsedHTML = $.load(html);
-            var eventItem = {
-                id: '',
-                date: '',
-                title: '',
-                venue: '',
-                city: ''
-            };
-            parsedHTML('.eventItem').map(function(i, item) {
-                var id = $(item).find('.party').attr('href');
-                var offset = id.indexOf("=");
-                if(offset != -1)
-                    id = id.substring(offset+1, id.length);
+new CronJob('*/59 * * * * *', function(next, res, req) {
+        url = config.get('scrapeResources')[0];
+        console.log(moment().format() + ' cron job for: ' + url);
+        request(url, function(err, resp, html){
+            if(err) {
+                throw err;
+            } else {
+                var items =[];
+
+                var parsedHTML = $.load(html);
                 var eventItem = {
-                    id: id,
-                    date: $(item).attr('date'),
-                    title: $(item).find('.party').text(),
-                    venue: $(item).children().first().next().text(),
-                    city: $(item).children().last().text()
+                    id: '',
+                    date: '',
+                    title: '',
+                    venue: '',
+                    city: ''
                 };
-                items.push(eventItem);
-            });
-        }
-        items = pretty.print(items);
+                parsedHTML('.eventItem').map(function(i, item) {
+                    var id = $(item).find('.party').attr('href');
+                    var offset = id.indexOf("=");
+                    if(offset != -1)
+                        id = id.substring(offset+1, id.length);
+                    var eventItem = {
+                        id: id,
+                        date: $(item).attr('date'),
+                        title: $(item).find('.party').text(),
+                        venue: $(item).children().first().next().text(),
+                        city: $(item).children().last().text()
+                    };
+                    items.push(eventItem);
+                });
+            }
+            next(pretty.print(items));
+        });
+    },function (items ) {
         var filename = 'scraped-events-' + moment().format() + '.json';
-        fs.writeFile('./data/' + filename, items, function(err){
+        var path = './data/scraped/';
+        console.log('postExcec called');
+
+        //postExecution
+        mkdirp(path, function(err) {
             if(err)
                 throw err;
-            else
-                console.log('File written at: /data/' + filename)
+            // path was created unless there was error
+            else {
+                fs.writeFile(path + filename, items, function (err) {
+                    console.log('File written at: ' + path);
+                });
+            }
         });
-    });
-},function () {
-    //postExecution
-}, true,
+    }, true,
     "Europe/Amsterdam"
 );
 
