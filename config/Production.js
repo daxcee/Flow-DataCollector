5b6a5ceacf070dd7ea5ecf329539c0c461e8233c
srmds <stevenramdas@gmail.com>
2015-06-06 23:44:32 +0200

    fix prod config

--- config/Production.js
+++ config/Production.js
@@ -1,7 +1,7 @@
 require('dotenv').load();
 
 module.exports = {
-    db_uri: 'mongodb://' + process.env.LOCAL_HOST + ':' + process.env.LOCAL_DBPORT + '/' + process.env.DB_NAME,
+    db_uri: 'mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASSWORD + '@' + process.env.DB_HOST + ':' + process.env.DB_PORT + '/' + process.env.DB_NAME,
     redirectURL:process.env.REDIRECT_URL,
     resources:
         [
